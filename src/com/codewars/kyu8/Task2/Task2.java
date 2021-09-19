5a3a2a3935ad5e066c37d9a9a8ff89eae9895bb4
MathewGvozdev <mathew15gen@gmail.com>
2021-09-19 13:37:03 +0300

    solution kyu8.Task2

--- src/com/codewars/kyu8/Task2/Task2.java
+++ src/com/codewars/kyu8/Task2/Task2.java
@@ -3,12 +3,12 @@ package com.codewars.kyu8.Task2;
 /**
  * Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer
  * greater than 0.
- *
+ * <p>
  * For example:
- *
+ * <p>
  * summation(2) -> 3
  * 1 + 2
- *
+ * <p>
  * summation(8) -> 36
  * 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
  **/
@@ -16,14 +16,23 @@ package com.codewars.kyu8.Task2;
 public class Task2 {
 
     public static void main(String[] args) {
-
+        System.out.println(GrassHopper.summation(2));
+        System.out.println(GrassHopper.summation(8));
+        System.out.println(GrassHopper.summation(20));
+        System.out.println(GrassHopper.summation(-5));
     }
 
     public class GrassHopper {
 
         public static int summation(int n) {
-
-            return 0;
+            int sum = 0;
+            if (n < 0) {
+                throw new IllegalArgumentException("Неверный аргумент, введите положительное число");
+            }
+            for (int i = 0; i <= n; i++) {
+                sum += i;
+            }
+            return sum;
         }
     }
 }
