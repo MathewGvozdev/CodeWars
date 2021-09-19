be8aba8f8da2e7fbcf59f80e88217d8b45d5e39b
MathewGvozdev <mathew15gen@gmail.com>
2021-09-19 19:35:32 +0300

    initial conditions kyu6.Task1

---
+++ src/com/codewars/kyu6/Task1/Task1.java
@@ -0,0 +1,29 @@
+package com.codewars.kyu6.Task1;
+
+/**
+ * Given a string of words, you need to find the highest scoring word.
+ *
+ * Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+ *
+ * You need to return the highest scoring word as a string.
+ *
+ * If two words score the same, return the word that appears earliest in the original string.
+ *
+ * All letters will be lowercase and all inputs will be valid.
+ **/
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public class Kata {
+
+        public static String high(String s) {
+            // Your code here...
+            return "";
+        }
+
+    }
+}
