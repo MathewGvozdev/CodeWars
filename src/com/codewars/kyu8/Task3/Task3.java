a8f828377c3f79e6e9df806447e032ba86966ff8
MathewGvozdev <mathew15gen@gmail.com>
2021-09-19 13:55:05 +0300

    solution (codewars acception) kyu8.Task3

--- src/com/codewars/kyu8/Task3/Task3.java
+++ src/com/codewars/kyu8/Task3/Task3.java
@@ -16,14 +16,32 @@ package com.codewars.kyu8.Task3;
 public class Task3 {
 
     public static void main(String[] args) {
-
+        System.out.println(BasicOperations.basicMath("+", 4, 7));
+        System.out.println(BasicOperations.basicMath("-", 15, 18));
+        System.out.println(BasicOperations.basicMath("*", 5, 5));
+        System.out.println(BasicOperations.basicMath("/", 49, 7));
     }
 
     public class BasicOperations
     {
         public static Integer basicMath(String op, int v1, int v2)
         {
-            return 0;
+            int result = 0;
+            switch (op) {
+                case "+":
+                    result = v1 + v2;
+                    break;
+                case "-":
+                    result = v1 - v2;
+                    break;
+                case "*":
+                    result = v1 * v2;
+                    break;
+                case "/":
+                    result = v1 / v2;
+                    break;
+            }
+            return result;
         }
     }
 }
