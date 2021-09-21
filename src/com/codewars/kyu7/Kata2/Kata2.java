2ceec1346dd15769658c20dfaf611172d2ea3185
MathewGvozdev <mathew15gen@gmail.com>
2021-09-22 00:28:57 +0300

    solution kyu7.Kata2

--- src/com/codewars/kyu7/Kata2/Kata2.java
+++ src/com/codewars/kyu7/Kata2/Kata2.java
@@ -1,7 +1,7 @@
 package com.codewars.kyu7.Kata2;
 
 /**
- *                          Credit Card Mask
+ * Credit Card Mask
  * Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most
  * secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on
  * your screen. Instead, we mask it.
@@ -14,17 +14,29 @@ package com.codewars.kyu7.Kata2;
  * // "What was the name of your first pet?"
  * Maskify.Maskify("Skippy");                                   // should return "##ippy"
  * Maskify.Maskify("Nananananananananananananananana Batman!"); // should return "####################################man!"
- * **/
+ **/
 
 public class Kata2 {
 
     public static void main(String[] args) {
-
+        System.out.println(Maskify.maskify("4556364607935616"));
+        System.out.println(Maskify.maskify("64607935616"));
+        System.out.println(Maskify.maskify("1"));
+        System.out.println(Maskify.maskify(""));
+        System.out.println(Maskify.maskify("Skippy"));
+        System.out.println(Maskify.maskify("Nananananananananananananananana Batman!"));
     }
 
     public class Maskify {
         public static String maskify(String str) {
-            throw new UnsupportedOperationException("Unimplemented");
+            String mask;
+            char[] symbols;
+            symbols = str.toCharArray();
+            for (int i = 0; i < symbols.length - 4; i++) {
+                symbols[i] = '#';
+            }
+            mask = String.valueOf(symbols);
+            return mask;
         }
     }
 }
